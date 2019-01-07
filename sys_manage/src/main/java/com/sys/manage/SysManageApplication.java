ee37ed375fc41f87e028d2cf15a258388443eb7d
sww <shiwenwei@do1.com.cn>
2019-01-07 16:06:24 +0800

    初始化

---
+++ sys_manage/src/main/java/com/sys/manage/SysManageApplication.java
@@ -0,0 +1,14 @@
+package com.sys.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysManageApplication.class, args);
+    }
+
+}
+
